a44dcac6c5d4345f61dff2e287bb478528dba8e1
Aniket Pandey <aniketpandey162003@gmail.com>
2026-05-31 19:52:36 +0530

    update: updated requirements.txt and embeddings.py

--- backend/app/services/embeddings.py
+++ backend/app/services/embeddings.py
@@ -1,3 +1,4 @@
+print("Gemini embeddings loaded")
 from google import genai
 from typing import List
 from app.config import GEMINI_API_KEY
@@ -17,8 +18,16 @@ def embed_Text(text: str) -> List[float]:
     return response.embeddings[0].values
 
 def embed_chunks(chunks: List[dict]) -> List[dict]:
+    
+    texts = [chunk["text"] for chunk in chunks]
+    response = client.models.embed_content(model=EMBED_MODEL, contents = texts)
     """
-ADD EMBEDDINGS TO EVERY CHUNK"""
-    for chunk in chunks:
-        chunk["embedding"] = embed_Text(chunk["text"])
+ADD Batch embeddings """
+
+    embeddings = [item.values 
+    for item in response.embeddings]
+    
+    for chunk, embedding in zip(chunks, embeddings):
+        chunk["embedding"] = embedding
     return chunks
+
